2b41799e259e74b72dc37686b059e6e97d10c642
Javier Leon <dev.javierleon@gmail.com>
2021-03-22 16:58:51 -0500

    twelve

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-// import PrimeraApp from './PrimeraApp';
-
+// import FirstApp from './FirstApp';
+//
 // import './index.css';
-
+//
 // const divRoot = document.querySelector('#app');
-
-// ReactDOM.render( <PrimeraApp />, divRoot);
+//
+// ReactDOM.render( <FirstApp />, divRoot);
 
 //---------------------------------------- <> Test
 import React from 'react';
 import CounterApp from './CounterApp';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 import './index.css';
 
 const divRoot = document.querySelector('#app');
 
 ReactDOM.render(<CounterApp value = {0} />, divRoot);
-
-
-
